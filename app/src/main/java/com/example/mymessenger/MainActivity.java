43d8f99af635e06c7353f6cc8efb0b88e295a0a5
Kholkin <kholkins@mail.ru>
2020-04-14 15:22:29 +0300

    main activity

--- app/src/main/java/com/example/mymessenger/MainActivity.java
+++ app/src/main/java/com/example/mymessenger/MainActivity.java
@@ -14,6 +14,9 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,10 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
     private String username;
 
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference().child("messages");
+        databaseReference.setValue("Hello, World!");
 
         username = "Default User";
 
