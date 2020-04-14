dc5dfdf1f6a2fa68583b4c6c584d547e3e8a879c
Kholkin <kholkins@mail.ru>
2020-04-14 10:02:06 +0300

    array adapter

--- app/src/main/java/com/example/mymessenger/MessageAdapter.java
+++ app/src/main/java/com/example/mymessenger/MessageAdapter.java
@@ -1,13 +1,31 @@
 package com.example.mymessenger;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
 
 public class MessageAdapter extends ArrayAdapter<AwesomeMessage> {
 
-    public MessageAdapter(@NonNull Context context, int resource) {
-        super(context, resource);
+    public MessageAdapter(@NonNull Context context, int resource, List<AwesomeMessage> messages) {
+        super(context, resource, messages);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if (convertView == null){
+            convertView = ((Activity)getContext()).getLayoutInflater().inflate(R.layout.message_item,parent,false);
+
+        }
+
+        return super.getView(position, convertView, parent);
     }
 }
