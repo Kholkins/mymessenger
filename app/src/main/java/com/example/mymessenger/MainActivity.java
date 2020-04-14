5e5dac62dae9c5ae0f260f7b61c6c8d37c2951f1
Kholkin <kholkins@mail.ru>
2020-04-14 13:05:12 +0300

    main activity

--- app/src/main/java/com/example/mymessenger/MainActivity.java
+++ app/src/main/java/com/example/mymessenger/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.mymessenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private MessageAdapter adapter;
+    private ProgressBar progressBar;
+    private ImageButton sendImageButton;
+    private Button sendMessageButton;
+    private EditText messageEditText;
+
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = "Default User";
+
+        listView = findViewById(R.id.messageListView);
+        progressBar = findViewById(R.id.messageProgressBar);
+        sendImageButton = findViewById(R.id.sendPhotoButton);
+        sendMessageButton = findViewById(R.id.sendMessageButton);
+        messageEditText = findViewById(R.id.messageEditText);
     }
 }
