88aa696f3ac6021ee5844fe94b24f1e6c0f27bbb
Kholkin <kholkins@mail.ru>
2020-04-15 19:05:28 +0300

    Authenticate

--- app/src/main/java/com/example/mymessenger/SignInActivity.java
+++ app/src/main/java/com/example/mymessenger/SignInActivity.java
@@ -3,12 +3,33 @@ package com.example.mymessenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText nameEditText;
+    private Button loginSignUpButton;
+    private TextView toggleLoginSignUpTextView;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        nameEditText = findViewById(R.id.nameEditText);
+        loginSignUpButton = findViewById(R.id.loginSignUpButton);
+        toggleLoginSignUpTextView = findViewById(R.id.toggleLoginSignUpTextView);
+
+        auth = FirebaseAuth.getInstance();
     }
 }
