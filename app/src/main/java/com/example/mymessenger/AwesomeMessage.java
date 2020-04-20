8a0f9ac43cfbbfcdf58db9a21cb8ec95b40068d7
Kholkin <kholkins@mail.ru>
2020-04-20 17:38:01 +0300

    Authenticate

--- app/src/main/java/com/example/mymessenger/AwesomeMessage.java
+++ app/src/main/java/com/example/mymessenger/AwesomeMessage.java
@@ -4,19 +4,49 @@ public class AwesomeMessage {
 
     private String text;
     private String name;
+    private String sender;
+    private String recipient;
     private String imageURL;
+    private boolean isMine;
 
     public AwesomeMessage() {
     }
 
-    public AwesomeMessage(String text, String name, String imageURL) {
+    public AwesomeMessage(String text, String name, String sender, String recipient, String imageURL, boolean isMine) {
         this.text = text;
         this.name = name;
+        this.sender = sender;
+        this.recipient = recipient;
         this.imageURL = imageURL;
+        this.isMine = isMine;
     }
 
     public String getText() {
         return text;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public boolean isMine() {
+        return isMine;
+    }
+
+    public void setMine(boolean mine) {
+        isMine = mine;
     }
 
     public void setText(String text) {
