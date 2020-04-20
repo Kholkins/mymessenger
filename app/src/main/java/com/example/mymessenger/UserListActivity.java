c93e3d4d548bcf34e1a33885549d57b61a1f133e
Kholkin <kholkins@mail.ru>
2020-04-20 15:47:47 +0300

    Authenticate

--- app/src/main/java/com/example/mymessenger/UserListActivity.java
+++ app/src/main/java/com/example/mymessenger/UserListActivity.java
@@ -1,14 +1,38 @@
 package com.example.mymessenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
 public class UserListActivity extends AppCompatActivity {
+
+
+    private String userName;
+
+    private FirebaseAuth auth;
+    private DatabaseReference usersDatabaseReference;
+    private ChildEventListener usersChildEventListener;
+
+    private ArrayList<User> userArrayList;
+    private RecyclerView userRecyclerView;
+    private UserAdapter userAdapter;
+    private RecyclerView.LayoutManager userLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
+        
+        buildReciclerView();
+    }
+
+    private void buildReciclerView() {
     }
 }
